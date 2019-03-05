99d92301ea1494441cf268e1f5891b4ce63c9a88
Anthony Da Mota <anthony.damota06@gmail.com>
2019-03-05 16:16:02 +0000

    Authentication

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      return context.prisma.links()
-    },
-    link: (parent, args) => links.find((link) => link.id === args.id)
-  },
-  Mutation: {
-    post: (root, args, context) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description,
-      })
-    },
-    updateLink: (parent, args) => {
-      const index = links.findIndex(link => link.id === args.id)
-      links[index] = {
-        ...links[index],
-        ...args
-      }
-      return links[index]
-    },
-    deleteLink: (parent, args) => {
-      links = links.filter(link => link.id !== args.id)
-      return args.id
-    }
-  },
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma },
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
